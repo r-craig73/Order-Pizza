60cf3577e52903f6ef49444ff67dbe693099f7a7
r-rcraig73 <ron.craig@comcast.net>
2018-01-26 15:55:06 -0800

    add check form for toppings and output toppings, non-object

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //business side
-function Pizza (size, toppings) {
+function Pizza (size, topping) {
   this.size = size;
-  this.toppings = toppings;
+  this.topping = topping;
 }
 
 Pizza.prototype.toppings = function() {
-  this.toppings.push(toppings);
+  this.toppings.push(topping);
   //console.log(pizza1.toppings);
 }
 
@@ -17,12 +17,22 @@ $(document).ready(function(){
     var size = $("input:radio[name=pizza]:checked").val();
     var sample = size;
     console.log(sample);
-    var toppings = "Cheese";
-    var pizza1 = new Pizza(size, toppings);
-
-
     $("#results").fadeIn();
-    $("#results").append(sample + " pizza with the following topings...");
+    $("#results").append(sample + " pizza with..." + "<br>");
+    // var toppings = "Cheese"
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var topping = $(this).val();
+      $("#results").append(topping + "<br>");
+      console.log(topping);
+    });
+
+
+    // var topping = "cheese";
+    // var size = "small";
+    // var pizza1 = new Pizza(size, topping);
+
+
+    // $("#results").append( )
 
     //pizza1.toppings(toppings);
   });
