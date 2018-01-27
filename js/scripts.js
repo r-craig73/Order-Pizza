63a95c8f18c4e63c3bbca760ca56391334f9ae38
r-rcraig73 <ron.craig@comcast.net>
2018-01-26 16:18:04 -0800

    add single form for topping, keep it simple

--- js/scripts.js
+++ js/scripts.js
@@ -12,28 +12,26 @@ Pizza.prototype.toppings = function() {
 
 // logic side
 $(document).ready(function(){
+
+
   $("#order").submit(function(event) {
     event.preventDefault();
     var size = $("input:radio[name=pizza]:checked").val();
-    var sample = size;
-    console.log(sample);
+    // var sample = size;
+    var topping = $("#toppings").val();
+    console.log(size);
+    console.log(topping);
     $("#results").fadeIn();
-    $("#results").append(sample + " pizza with..." + "<br>");
-    // var toppings = "Cheese"
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      var topping = $(this).val();
-      $("#results").append(topping + "<br>");
-      console.log(topping);
-    });
+    $("#results").append(size + " pizza with..." + "<br>");
+    $("#results").append(topping + "<br>");
+
 
 
     // var topping = "cheese";
     // var size = "small";
     // var pizza1 = new Pizza(size, topping);
-
-
     // $("#results").append( )
-
     //pizza1.toppings(toppings);
+
   });
 });
