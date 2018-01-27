42d71509bbf4bdcc22422af2a691065389fc0003
r-rcraig73 <ron.craig@comcast.net>
2018-01-26 16:43:24 -0800

    add prototype, but cannot output the value

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,39 @@
 //business side
-function Pizza (size, topping) {
+function Pizza (size, topping, finalCost) {
   this.size = size;
   this.topping = topping;
+  this.finalCost = 0;
 }
 
-Pizza.prototype.toppings = function() {
-  this.toppings.push(topping);
-  //console.log(pizza1.toppings);
-}
+Pizza.prototype.price = function() {
+  if (this.size === "Small") {
+    this.finalCost = 10;
+    console.log(this.finalCost);
+  } else if (this.size === "Medium") {
+    this.finalCost = 12;
+    console.log(this.finalCost);
+  } else {
+    this.finalCost = 14;
+    console.log(this.finalCost);
+  }
+};
 
 
 // logic side
 $(document).ready(function(){
-
 
   $("#order").submit(function(event) {
     event.preventDefault();
     var size = $("input:radio[name=pizza]:checked").val();
     // var sample = size;
     var topping = $("#toppings").val();
+    var pizza1 = new Pizza(size, topping);
     console.log(size);
     console.log(topping);
     $("#results").fadeIn();
     $("#results").append(size + " pizza with..." + "<br>");
     $("#results").append(topping + "<br>");
-
-
-
-    // var topping = "cheese";
-    // var size = "small";
-    // var pizza1 = new Pizza(size, topping);
-    // $("#results").append( )
-    //pizza1.toppings(toppings);
+    $("#results").append("Final price " + pizza1.finalCost);
 
   });
 });
