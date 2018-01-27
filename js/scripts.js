49434b4376e2cfddcab8277a5273688146679fd7
r-rcraig73 <ron.craig@comcast.net>
2018-01-26 17:00:16 -0800

    final commit for Friday

--- js/scripts.js
+++ js/scripts.js
@@ -5,35 +5,39 @@ function Pizza (size, topping, finalCost) {
   this.finalCost = 0;
 }
 
-Pizza.prototype.price = function() {
+Pizza.prototype.finalPrice = function() {
   if (this.size === "Small") {
     this.finalCost = 10;
     console.log(this.finalCost);
   } else if (this.size === "Medium") {
     this.finalCost = 12;
     console.log(this.finalCost);
-  } else {
+  } else if (this.size === "Large") {
     this.finalCost = 14;
+    console.log(this.finalCost);
+  } else {
+    this.finalCost = 400;
     console.log(this.finalCost);
   }
 };
 
 
 // logic side
-$(document).ready(function(){
+$(document).ready(function() {
 
   $("#order").submit(function(event) {
     event.preventDefault();
     var size = $("input:radio[name=pizza]:checked").val();
-    // var sample = size;
+    var size = String(size);
     var topping = $("#toppings").val();
-    var pizza1 = new Pizza(size, topping);
+    var pizza1 = new Pizza (size, topping);
     console.log(size);
     console.log(topping);
     $("#results").fadeIn();
     $("#results").append(size + " pizza with..." + "<br>");
     $("#results").append(topping + "<br>");
-    $("#results").append("Final price " + pizza1.finalCost);
+    $("#results").append("Final Price "+ "<br>");
+    $("#results").append(pizza1.finalPrice);
 
   });
 });
